a4aa26f4c270e298221105bb8460aa087648f141
Luca Balado <lucabalado@gmail.com>
2023-09-05 15:51:31 -0300

    Actividad objetos 2

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function createObject(name, subs) {
-  const canal = {
-    name: null,
-    subs: 0,
-    hash: name.length * subs,
-    getStatus: function (name, subs) {
-      const mensaje = `el canal ${name} tiene ${subs} suscriptores`;
-      return mensaje;
-    },
-  };
-  return canal;
-}
+/*
+1)
+Lo que ejecutaría la consola en el primer códigos seria:"70  180" ya que, agrega las
+propiedades de peso y altura con dichos valores, y los imprime únicamente a ellos
+*/
 
-const persona = createObject("luca", 100);
-console.log(persona);
+/*
+2)
+Lo que imprimiría la consola en el segundo codigo seria un error, ya que pide imprimir una
+propiedad la cual ya no existe debido a que previamente fue renombrada.
+*/
+
+/*
+3)
+El nombre del atajo que nos permite recuperar el valor de una propiedad de un objeto
+es: "Destructuración"
+*/
