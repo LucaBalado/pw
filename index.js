72f412ff4cc24fd34bb0e16837e07d6920334e85
Luca Balado <lucabalado@gmail.com>
2023-09-05 15:11:17 -0300

    actividad objetos 1

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-let numbers = [3, -1, 9, -12, -18];
-function sortAbsoluteNumbers(numeros) {
-  let numabs = [];
-  for (let i of numeros) {
-    const numero = Math.abs(i);
-    numabs.push(numero);
-  }
-  const order = numabs.sort(function (a, b) {
-    return a - b;
-  });
-  return order;
+function createObject(name, subs) {
+  const canal = {
+    name: null,
+    subs: 0,
+    hash: name.length * subs,
+    getStatus: function (name, subs) {
+      const mensaje = `el canal ${name} tiene ${subs} suscriptores`;
+      return mensaje;
+    },
+  };
+  return canal;
 }
-console.log(sortAbsoluteNumbers(numbers));
 
-/**/
-
-let words = [
-  "hola",
-  "tres",
-  "uno",
-  "Universidad",
-  "de",
-  "Buenos Aires",
-  "pepito",
-  "celular",
-];
-let word = "pepito";
-
-function buscaPalabras(words, word) {
-  //const palabras = words.filter(length());
-  const palabra = words.indexOf(word);
-  let letras = [];
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > palabra) {
-      letras.push(words[i]);
-    }
-  }
-  console.log(letras);
-}
-buscaPalabras(words, word);
+const persona = createObject("luca", 100);
+console.log(persona);
