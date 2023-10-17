9f5708ad7de4ac695a1858da5aa3c517c2e7539f
Luca Balado <lucabalado@gmail.com>
2023-10-17 17:04:57 -0300

    Rick And Morty API continuacion

--- vite-project/src/components/characters/Characters.jsx
+++ vite-project/src/components/characters/Characters.jsx
@@ -1,50 +1,41 @@
+import { useState } from "react";
+
 export const Characters = () => {
-  const personajes = [
-    {
-      id: 1,
-      name: "Rick Sanchez",
-      status: "Alive",
-      species: "Human",
-      type: "",
-      gender: "Male",
-      origin: {
-        name: "Earth",
-      },
-      location: {
-        name: "Citadel of Ricks",
-      },
-      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    },
-    {
-      id: 2,
-      name: "Morty Smith",
-      status: "Alive",
-      species: "Human",
-      type: "",
-      gender: "Male",
-      origin: {
-        name: "Earth",
-      },
-      location: {
-        name: "Citadel of Ricks",
-      },
-      image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    },
-  ];
+  //hooks(ganchos) -> sirven para manipular los datos
+  //useState -> podemos guardar un valor y setearle un nuevo valor -> const [valor, setValor] = useState([])
+  //useEffect -> ejecutamos el codigo en el momento en el que lo necesitamos
+  /*
+      useState(() => {
+          se va a ejecutar el codigo
+      },[])
+      si entre los corchetes no le pasamos nada, se ejecuta solo al principio, y si no, cada vez que se actualice la variable que le pasamos   
+  */
+
+  const [characters, setCharacters] = useState([]);
+
+  useState(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
 
   //llaves dentro del return para poder meter codigo js
   return (
     <>
-      {personajes.map((item, index) => (
+      {characters.map((item, index) => (
         <div className="tarjeta" key={item.id}>
           <img className="foto" src={item.image} />
           <div className="info">
             <p className="nombre">{item.name}</p>
             <p className="datos">
-              {item.status}-{item.species}
+              <span className="status_icon"></span>
+              {item.status} - {item.species}
             </p>
             <p className="planeta">{item.origin.name}</p>
+            <h4>Last known location:</h4>
             <p className="localizacion">{item.location.name}</p>
+            <h4>First seen in:</h4>
+            <p className="firstSeen">-------------</p>
           </div>
         </div>
       ))}
