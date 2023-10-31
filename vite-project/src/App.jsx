a49608292fba5f8ec8df0803f36b77d0906b0cb7
Luca Balado <lucabalado@gmail.com>
2023-10-31 16:59:46 -0300

    seguimos con la api de Rick & Morty

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,43 @@
 import { Characters } from "./components/characters/Characters";
 import "./app.css";
+import Svg from "./components/Svg";
 
 function App() {
   return (
     <>
-      <div className="encabezado">
-        <h1>The Rick and Morty API</h1>
-      </div>
-      <div className="grilla">
-        <Characters />
-      </div>
+      <header className="header">
+        <nav>
+          <ul className="header_list">
+            <li>
+              <a className="documentos" href="/documentation">
+                Docs
+              </a>
+            </li>
+            <li>
+              <a className="about" href="/about">
+                About
+              </a>
+            </li>
+            <li>
+              <span className="apoyar">Support Us</span>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className="encabezado">
+          <h1>The Rick and Morty API</h1>
+          <div>
+            <Svg />
+          </div>
+        </div>
+        <div className="grilla">
+          <Characters />
+        </div>
+      </main>
+      <footer className="pie">
+        <p className="creditos">by Luca Balado 2023</p>
+      </footer>
     </>
   );
 }
